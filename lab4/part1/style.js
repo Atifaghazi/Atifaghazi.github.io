ce9a91973155920538d0f5c4999e0329307f107f
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-16 16:17:39 -0400

    Update style.js
    delete the stylesheet

--- lab4/part1/style.js
+++ lab4/part1/style.js
@@ -1,7 +1,7 @@
 /*  Name: Atifa Ghazi
     File: style.js
     Date: 16 March 2026
-    stylesheet JavaScript for generating a random silly story! - lab 4 part 1 page
+    JavaScript for generating a random silly story! - lab 4 part 1 page
  */
 
 // Complete variable definitions and random functions
