9ec9300995a256c14c2693c54625805947cfcb26
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-16 20:55:06 -0400

    Update main.js
    Change the name of const to insert x insert y insertz as per requirement for lab4 part1

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -17,9 +17,9 @@ function randomValueFromArray(array) {
 
 // Raw text strings
 
-const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const places = ["the soup kitchen", "Disneyland", "the White House"];
-const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+const insertY = ["the soup kitchen", "Disneyland", "the White House"];
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
 
 // Partial return random string function
 
