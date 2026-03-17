dd9eb2492ae3da578912f4542f68c5d7cf5c24ea
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-16 21:00:02 -0400

    Update main.js
    Added some changes for the const and also added the space to it

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -25,7 +25,7 @@ const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewal
 
 function returnRandomStoryString() {
 
-const storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. 
+  const storyText = `It was 94 fahrenheit outside, so :insertx: went for a walk. 
 When they got to :inserty:, they stared in horror for a few moments, 
 then :insertz:. Bob saw the whole thing, but was not surprised — 
 :insertx: weighs 300 pounds, and it was a hot day.`;
@@ -58,7 +58,6 @@ function generateStory() {
     const temperature = Math.round((94 - 32) * 5 / 9) + " centigrade";
     newStory = newStory.replaceAll("300 pounds", weight);
     newStory = newStory.replaceAll("94 fahrenheit", temperature);
- 
   }
 
   // TODO: replace "" with the correct expression
