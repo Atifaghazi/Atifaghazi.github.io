7478edee405398a05494cd16b54f0bee865714c2
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-17 17:39:29 -0400

    Update main.js
    Added Implemented a loop to dynamically generate gallery thumbnails with click event listeners for updating the main display.

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -17,3 +17,18 @@ const images = [
   { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
   { filename: "pic5.jpg", alt: "Large moth on a leaf" },
 ];
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+
+newImage.setAttribute("src", `images/${image.filename}`);
+  newImage.setAttribute("alt", image.alt);
+
+thumbBar.appendChild(newImage);
+
+newImage.addEventListener("click", (e) => {
+    displayedImage.src = e.target.src;
+    displayedImage.alt = e.target.alt;
+  });
+}
+
