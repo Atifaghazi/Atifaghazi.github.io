ba57b2d4919f7ae2684dcd6370f98c334b173be6
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-17 16:51:26 -0400

    Update main.js
    Starting point of the main.js for lab4 part 2 as per requirement

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -3,3 +3,9 @@
     Date: 17 March 2026
     JavaScript for generating a Image gallery! - lab 4 part 2 page
  */
+
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
