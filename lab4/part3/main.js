bca7670404c992011bfa9a12b13afa399715e928
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-19 12:09:58 -0400

    Update main.js
    Added object for description and start point main.js for lab4 part3 as per requirement

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -1,6 +1,12 @@
 /*  Name: Atifa Ghazi
     File: main.js
     Date: 19 March 2026
-    JavaScript for generating a Bouncing balls! - lab 4 part 3 page
+    JavaScript for Object building practice generating a Bouncing balls! - lab 4 part 3 page
  */
+
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
 
