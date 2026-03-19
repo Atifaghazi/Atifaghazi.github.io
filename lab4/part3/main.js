257759df22c9c7d756735630e80d0989ead91889
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-19 12:11:37 -0400

    Update main.js
    Implemented a helper function to calculate random integers within a specific range for ball properties.

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -10,3 +10,8 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
