7f573092303f27f9a347d0240f31dafb2e1a1ace
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-23 13:48:32 -0400

    Update main.js
    starting point of the main.js for lab4 part4 as per requirement

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -4,3 +4,10 @@
     JavaScript for Adding features to our bouncing balls demo! - lab 4 part 4 page
  */
 
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const para = document.querySelector("p");
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
