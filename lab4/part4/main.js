2608c87cd38bd3cbe1206c02bfc92731b8b80f7d
Atifaghazi <atifa.ghazi@dcmail.ca>
2026-03-23 13:50:20 -0400

    Update main.js
    Initialized the global ball counter and utility functions for generating random ranges and RGB color strings.

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -11,3 +11,13 @@ const para = document.querySelector("p");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
+let count = 0;
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomRGB() {
+  return `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+}
+
